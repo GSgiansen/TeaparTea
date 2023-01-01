fe2bbadce2c50c62b1c9ee13a3129ca41eebc203
GianSen <giansenrockz@gmail.com>
2023-01-01 23:08:56 +0800

    Added about page

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
+import s from "../styles/profile.css"
 const about = () => {
   return (
-    <div>about</div>
+    <div className='container'>
+    <Navbar/>
+    <div className='text'>
+      This is Gian Sen's cvwo project! there are still many features yet to be implemented so do keep a lookout for it!
+    </div>
+    </div>
   )
 }
 
