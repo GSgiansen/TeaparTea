223d40e0b17f003641dbe0bf0ee03ed98d5d8443
GianSen <giansenrockz@gmail.com>
2023-01-24 17:45:56 +0800

    Changed number of workers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   #get 'welcome/index'
+  root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
