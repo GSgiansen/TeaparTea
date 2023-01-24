e3b80cda4d3a6176a95f7441283203819f674da4
GianSen <giansenrockz@gmail.com>
2023-01-24 17:00:35 +0800

    Home page display

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   #get 'welcome/index'
-  root "home#frontend/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
